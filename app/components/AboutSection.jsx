907054dd71673bd0da5cf88996fd976051ab4fea
jeffrey <pengtuzhang@gmail.com>
2023-12-13 08:34:02 +1000

    trying to debug nav bar not working after deployment

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -54,7 +54,7 @@ const AboutSection = () => {
     }
 
   return (
-    <section className='text-white' id='about'>
+    <section className='text-white about' id='about'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
             <Image src = {'/images/about-image.png'} alt='about image' height={500} width={500}/>
             <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
--- app/components/Connect.jsx
+++ app/components/Connect.jsx
@@ -9,7 +9,7 @@ const Connect = () => {
   return (
     <section
       id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative contact"
     >
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
