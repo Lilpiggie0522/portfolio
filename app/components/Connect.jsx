da77a8d3dade4f6c2c520e716e48a2dfae0f96d8
jeffrey <pengtuzhang@gmail.com>
2023-12-13 06:17:26 +1000

    Finalised copy to clip board button and implemented its component

--- app/components/Connect.jsx
+++ app/components/Connect.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import GithubIcon from '@/public/github-icon.svg'
 import LinkedinIcon from '@/public/linkedin-icon.svg'
+import CopyToClip from './CopyToClip'
 
 const Connect = () => {
   return (
@@ -14,14 +15,24 @@ const Connect = () => {
         <h5 className="text-xl font-bold text-white my-2">
           Let&apos;s Connect
         </h5>
+
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
         </p>
+
+        <p className='text-[#ADB7BE] mb-4 max-w-md font-bold text-sm'>
+          Please note that the following Linkedin
+          Icon does not work as I have no Linkedin profile and have no intention
+          to make one.
+        </p>
+        <div className='text-white mb-4 max-w-md text-md font-bold'>
+          <CopyToClip />
+        </div>
+      
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com">
+          <Link href="https://github.com/Lilpiggie0522">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="https://linkedin.com">
---
+++ app/components/CopyToClip.jsx
@@ -0,0 +1,29 @@
+'use client'
+import React, { useState } from 'react'
+import copy from 'clipboard-copy';
+
+const CopyToClip = ({ textToCopy }) => {
+    const [isCopied, setIsCopied] = useState(false);
+  
+    const handleCopyClick = async () => {
+      try {
+        await copy(textToCopy);
+        setIsCopied(true);
+      } catch (err) {
+        console.error('Error copying to clipboard', err);
+      }
+  
+      // Reset the "Copied" state after a brief delay
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1500);
+    };
+  
+    return (
+        <button onClick={handleCopyClick} disabled={isCopied}>
+            {isCopied ? 'Copied!' : 'pengtuzhang@gmail.com'}
+        </button>
+    );
+  };
+
+export default CopyToClip
